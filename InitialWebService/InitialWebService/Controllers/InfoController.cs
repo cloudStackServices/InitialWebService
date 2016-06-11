2237224fca4d0d4334d5d0bfae0c301dae5ca190
kklinger <kklinger@causeview.com>
2016-06-11 12:48:15 -0400

    Add Model and controller code to fetch Object contents

--- InitialWebService/InitialWebService/Controllers/InfoController.cs
+++ InitialWebService/InitialWebService/Controllers/InfoController.cs
@@ -5,24 +5,27 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+using InitialWebService.Models;
+
 namespace InitialWebService.Controllers
 {
     public class InfoController : ApiController
     {
         // GET api/info
-        //public IEnumerable<string> Get()
+        // public IEnumerable<string> Get()
 
-        // www.klingerdev.com/Info/api/info returns {"id":"1","content":"Hello World"}
+        // www.klingerdev.com/Info/api/Info returns {"id":"1","content":"Hello World"}
+        // local/api/Info returns {"id":"1","content":"Hello World"}
 
-  //      public InfoObject Get()
-  //      {
-  //          var myModel = new InfoObject();
-  //          myModel.id = "1";
-  //          myModel.content = "Hello World";
+        public InitialWebServiceObject Get()
+              {
+                  var myModel = new InitialWebServiceObject();
+                  myModel.id = "1";
+                  myModel.content = "Hello World";
 
-  //           return myModel;
-  //          // return new string[] { "value1", "value2" };
-  //      }
+                  return myModel;
+        //          // return new string[] { "value1", "value2" };
+              }
 
         // GET api/info/5
         public string Get(int id)
---
+++ InitialWebService/InitialWebService/Models/InitialWebService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InitialWebService.Models
+{
+    public class InitialWebServiceObject
+    {
+        public string id;
+        public string content;
+    }
+}
